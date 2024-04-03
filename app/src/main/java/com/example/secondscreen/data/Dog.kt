5bc21f8c79b6d48018310fe8b814a0f4dc86c641
Cristina Jung <cristijung@gmail.com>
2024-04-03 17:57:08 -0300

    textos e data class

--- app/src/main/java/com/example/secondscreen/data/Dog.kt
+++ app/src/main/java/com/example/secondscreen/data/Dog.kt
@@ -18,5 +18,6 @@ val dogs = listOf(
     Dog(R.drawable.aoi, R.string.dog_name_6, 14, R.string.dog_description_6),
     Dog(R.drawable.moana, R.string.dog_name_7, 2, R.string.dog_description_7),
     Dog(R.drawable.otto, R.string.dog_name_8, 7, R.string.dog_description_8),
-    Dog(R.drawable.dobby, R.string.dog_name_9, 4, R.string.dog_description_9)
+    Dog(R.drawable.dobby, R.string.dog_name_9, 4, R.string.dog_description_9),
+    Dog(R.drawable.bastiao, R.string.dog_name_10, 4, R.string.dog_description_10)
 )
